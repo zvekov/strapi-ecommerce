4412d8b990407f0ed62c6d5714a6754a842d87cb
zvekov <mail@zvekov.com>
2021-03-30 21:22:04 +0300

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '00db7f1ed805ed9a74655cf3e7681fb6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '82866b23-fdec-4894-b7da-5484d48f23aa'
+};
