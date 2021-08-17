11e79a7af0ec210ec0b989f2eac76932229966ab
zvekov <mail@zvekov.com>
2021-08-17 17:04:01 +0300

    fix config server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron: { enabled: false },
+  url: 'https://nuxt-storeback-strapi.herokuapp.com',
   admin: {
+    autoOpen: false,
     auth: {
       secret: env('ADMIN_JWT_SECRET', '00db7f1ed805ed9a74655cf3e7681fb6'),
     },
